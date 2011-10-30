6c70868a7dd2db1d5db5faf330dbdf4417e31d96
Sridhar Srinivasan <sridharsrinivasan86@gmail.com>
2011-10-30 03:49:36 -0400

    after writing timer driver, time syscall, wired in irq handler, all working

---
+++ kernel/syscalls/ktime.c
@@ -0,0 +1,28 @@
+/*
+ * ktime.c: Implementation of the time syscall 
+ * Authors: Sridhar Srinivasan <sridhar1@andrew.cmu.edu>
+ *          Ramya Bolla <rbolla@andrew.cmu.edu>
+ *          Vinay Prasad <vinayp1@andrew.cmu.edu>
+ *
+ */
+
+#include <exports.h>
+#include <bits/errno.h>
+#include <bits/fileno.h>
+
+#define TIMER_INTR_INTERVAL 10
+
+/*
+ * globals
+ */
+extern unsigned long num_ticks;
+
+/*
+ * implementation of the time syscall
+ * @param: void 
+ * @return unsigned long - time in milliseconds since bootup 
+ */
+unsigned long ktime()
+{
+	return (num_ticks * TIMER_INTR_INTERVAL);
+}
